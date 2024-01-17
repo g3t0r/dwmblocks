171fbf6fba27ae927452408022dd19a3b0f5d556
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2024-01-17 18:12:46 +0100

    added custom config

--- config.h
+++ config.h
@@ -2,33 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
-	/* {"",	"sb-music",	0,	11}, */
-	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
-	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
-	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-	/* {"",	"sb-iplocate", 0,	27}, */
-	{"",	"sb-help-icon",	0,	15},
+  {" ", "curl wttr.in/Krakow?format=3 -s | awk -e '{print $2 \" \" $3}'", 120, 0},
+  {"VOL: ", "amixer -D pulse sget Master | awk -F\"[][]\" '/Left:/ { print $2 \" \" $4}'", 0, 0},
+  {"", "date", 5, 0}
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
